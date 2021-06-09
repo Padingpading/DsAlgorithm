1e7040418e384baa0c332e5afd5c0a123c59ee94
libin <449196538@qq.com>
2021-06-09 22:53:23 +0800

    测试

---
+++ src/main/java/com/padingpading/test/Test.java
@@ -0,0 +1,13 @@
+package com.padingpading.test;
+
+/**
+ * @author libin
+ * @description
+ * @date 2021/6/9
+ */
+public class Test {
+
+    public static void main(String[] args) {
+        System.out.println("Test");
+    }
+}
